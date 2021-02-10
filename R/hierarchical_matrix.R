927c7601855bb5c19abe768d994fab6b01179e90
yForecasting <yves.sagaert@gmail.com>
2021-02-10 15:33:16 +0100

    fix warning is.na() procedure in if clause by class()

--- R/hierarchical_matrix.R
+++ R/hierarchical_matrix.R
@@ -1,14 +1,14 @@
-hierarchical_matrix <- function(nodes, levels=NA) {
+hierarchical_matrix <- function(nodes, nodeshier=NA) {
   # make summing matrix S for reconcilation formula
   # First all low level series will be summed to the top series
   # Then the lowest level series are all set to 1 in identity matrix format
   # Then intermediate aggregations are formed in-between
-  if (is.na(levels)){
-    # Assume 3-level hierarchy if no levels are given
-    levels=rep(2,length(nodes))
+  if (class(nodeshier)=="logical"){
+    # Assume 3-level hierarchy if no nodeshier are given
+    nodeshier=rep(2,length(nodes))
   }
-  l2 <- nodes[levels==2]
-  s <- matrix(0,ncol=sum(l2),nrow=(1+sum(l2)+length(nodes[levels>1])))
+  l2 <- nodes[nodeshier==2]
+  s <- matrix(0,ncol=sum(l2),nrow=(1+sum(l2)+length(nodes[nodeshier>1])))
   # Top level
   s[1,]=1
   # Lowest level (identity)
@@ -16,12 +16,12 @@ hierarchical_matrix <- function(nodes, levels=NA) {
   for (irow in (f1):nrow(s)){
     s[irow,(irow-f1+1)] <- 1
   }
-  # Middle levels
+  # Middle level - with nodeshier
   irow = 2
   icol = 0
-  lev0 = levels[1]
+  lev0 = nodeshier[1]
   for (i in 1:length(nodes)){
-    if (lev0 !=levels[i]){
+    if (lev0 !=nodeshier[i]){
       # or (icol==ncol(s))
       icol=0
     }
@@ -30,7 +30,7 @@ hierarchical_matrix <- function(nodes, levels=NA) {
       s[irow,icol] <- 1
     }
     irow <- irow + 1
-    lev0 <- levels[i]
+    lev0 <- nodeshier[i]
   }
   return(s)
 }
